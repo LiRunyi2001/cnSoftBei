99b0e998087f6676bf0044fd89d7f93c3de40ef8
magicgh <magicgher@hotmail.com>
2021-06-07 12:19:59 +0800

    :sparkles: Add GUI for FastText

---
+++ FastText/GUI/main.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+import fasttext
+# Define the window's contents
+layout = [[sg.Text("请输入待测新闻", font='微软雅黑')],
+          [sg.Multiline(size=(40, 20), key='-INPUT-', font='微软雅黑')],
+          [sg.Text(size=(40, 1), key='-OUTPUT-', font='微软雅黑')],
+          [sg.Button('预测', font='微软雅黑'), sg.Button('退出', font='微软雅黑')]]
+
+# Create the window
+window = sg.Window('新闻分类', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    label_map = {"1": "财经", "2": "房产", "3": "教育", "4": "科技", "5": "军事", "6": "汽车", "7": "体育", "8": "游戏", "9": "娱乐", "10": "其他"}
+    model = fasttext.load_model("./model.bin")
+    result = label_map[model.predict(values['-INPUT-'].strip())[0][0].split("__")[-1]]
+    window['-OUTPUT-'].update(f"分类结果：{result}")
+
+# Finish up by removing from the screen
+window.close()
--- FastText/README.md
+++ FastText/README.md
@@ -1,18 +1,24 @@
 # Fast Text
+
 ## Description
-A very simple and fast method used to classify news.   
+
+A very simple and fast method used to classify news.
 Precision and Recall: approximate 88% on the Toutiao Dataset
+
 ## Usage
+
 **Only support querying one sentence at a time**  
 
-0. Install dependencies
+0. Install dependencies.
 
    ```shell
    pip install requirements.txt
    ```
+
 1. Download the pretrained model from [here](https://1drv.ms/u/s!AmRrl2CAWm_2hNhj7Mu-V2Ku15dQKg?e=zxg1cA) and move it to the root directory.
 2. Modify the value of `query_words` as the sentence you want to query in `predict.py`.
 3. Run `predict.py` and it will print the label according to `toutiao_model.bin`.
+
     ```shell
    python -u predict.py
    ```
